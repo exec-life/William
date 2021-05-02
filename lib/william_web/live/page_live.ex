28c061bd31a0fd5be6b18f599ea899c4d3dfd555
TheDiplomats <socx@protonmail.com>
2021-05-02 07:13:11 -0700

    Added log in page and registration

--- lib/william_web/live/page_live.ex
+++ lib/william_web/live/page_live.ex
@@ -13,4 +13,18 @@ defmodule WilliamWeb.PageLive do
       |> assign(trigger_submit: false)}
   end
 
+  @impl true
+  def handle_event("validate", %{"user" => user_params}, socket) do
+    changeset =
+      %User{}
+      |> User.registration_changeset(user_params)
+      |> Map.put(:action, :validate)
+    :timer.sleep(9000)
+    {:noreply, socket |> assign(changeset: changeset)}
+  end
+
+  def handle_event("save", _, socket) do
+    {:noreply, assign(socket, trigger_submit: true)}
+  end
+
 end
